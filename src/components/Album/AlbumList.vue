8092acc3ccb8530b88f89b78b77debc86b3055b8
gahgsp <kelvinbianchi@gmail.com>
2021-11-06 11:30:34 -0300

    Moved the responsibility to load the albums to the albums page instead of loading them in the app component.

--- src/components/Album/AlbumList.vue
+++ src/components/Album/AlbumList.vue
@@ -31,7 +31,6 @@ export default {
   },
   methods: {
     ...mapActions({
-      loadAllAlbums: 'loadAlbums',
       addNewFavoriteAlbum: 'addFavorite',
       removeFavoriteAlbum: 'removeFavorite',
     }),
