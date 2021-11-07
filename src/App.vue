248711fe7e05fc62cf84f96c9b1f05e5fb1daafa
gahgsp <kelvinbianchi@gmail.com>
2021-11-07 12:01:49 -0300

    Moved the navigation from the main app component to a shared navigation component.

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,6 @@
 <template>
   <div id="app">
-    <nav
-      class="navbar has-shadow"
-      role="navigation"
-      aria-label="main navigation"
-    >
-      <div class="navbar-menu">
-        <router-link v-if="!isLoggedIn" to="/" class="navbar-item"
-          >Home</router-link
-        >
-        <router-link v-if="isLoggedIn" to="/albums" class="navbar-item"
-          >Albums</router-link
-        >
-        <router-link v-if="isLoggedIn" to="/favorites" class="navbar-item"
-          >My Favorites</router-link
-        >
-        <router-link to="/about" class="navbar-item">About</router-link>
-      </div>
-      <div class="navbar-end">
-        <div class="navbar-item">
-          <div class="buttons">
-            <button v-if="isLoggedIn" @click="logout" class="button is-link">
-              Logout
-            </button>
-          </div>
-        </div>
-      </div>
-    </nav>
+    <navigation></navigation>
     <transition name="slide-fade" mode="out-in">
       <router-view />
     </transition>
@@ -34,26 +8,11 @@
 </template>
 
 <script>
-import { mapActions, mapGetters } from 'vuex';
+import Navigation from './components/shared/Navigation.vue';
 
 export default {
   name: 'app',
-  computed: {
-    ...mapGetters({
-      isUserLoggedIn: 'isLoggedIn',
-    }),
-    isLoggedIn() {
-      return this.isUserLoggedIn;
-    },
-  },
-  methods: {
-    ...mapActions({
-      logOutUser: 'logOut',
-    }),
-    logout() {
-      this.logOutUser();
-    },
-  },
+  components: { Navigation },
 };
 </script>
 
@@ -64,17 +23,6 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
 }
 
 .slide-fade-enter-active {
---
+++ src/components/shared/Navigation.vue
@@ -0,0 +1,62 @@
+<template>
+  <nav class="navbar has-shadow" role="navigation" aria-label="main navigation">
+    <div class="navbar-menu">
+      <router-link v-if="!isLoggedIn" to="/" class="navbar-item"
+        >Home</router-link
+      >
+      <router-link v-if="isLoggedIn" to="/albums" class="navbar-item"
+        >Albums</router-link
+      >
+      <router-link v-if="isLoggedIn" to="/favorites" class="navbar-item"
+        >My Favorites</router-link
+      >
+      <router-link to="/about" class="navbar-item">About</router-link>
+    </div>
+    <div class="navbar-end">
+      <div class="navbar-item">
+        <div class="buttons">
+          <button v-if="isLoggedIn" @click="logout" class="button is-link">
+            Logout
+          </button>
+        </div>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script>
+import { mapActions, mapGetters } from 'vuex';
+
+export default {
+  name: 'navigation',
+  computed: {
+    ...mapGetters({
+      isUserLoggedIn: 'isLoggedIn',
+    }),
+    isLoggedIn() {
+      return this.isUserLoggedIn;
+    },
+  },
+  methods: {
+    ...mapActions({
+      logOutUser: 'logOut',
+    }),
+    logout() {
+      this.logOutUser();
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+#nav {
+  padding: 30px;
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
+}
+</style>
